ddc4ae116b5ebff53de476bed4cc85404b6cf23a
Marek Rogala <mrogala@katalogmarzen.pl>
2024-02-09 18:14:19 +0100

    add heading

--- src/components/atoms/ProductDescription.tsx
+++ src/components/atoms/ProductDescription.tsx
@@ -10,8 +10,8 @@ export const ProductDescription = ({ category, name, price }: ProductDescription
 	return (
 		<div className="flex flex-col gap-2 pt-2 text-xs">
 			<div className="flex justify-between">
-				<p>{name}</p>
-				<p>{priceFormat(price / 100)}</p>
+				<h6>{name}</h6>
+				<h6>{priceFormat(price / 100)}</h6>
 			</div>
 			<p className="text-gray-500">{category}</p>
 		</div>
